6a3b068e604af314126bdc3220b330d9ab241533
hari shankar <harishankar.rajeev@gmail.com>
2022-05-28 11:41:40 -0500

    Hello what's up

--- src/routes/sidebar.js
+++ src/routes/sidebar.js
@@ -9,7 +9,7 @@ const routes = [
   {
     path: '/app/dashboard', // the url
     icon: 'HomeIcon', // the component being exported from icons/index.js
-    name: 'Dashboard', // name that appear in Sidebar
+    name: 'Statistics', // name that appear in Sidebar
   },
   {
     path: '/app/forms',
@@ -21,53 +21,7 @@ const routes = [
     icon: 'CardsIcon',
     name: 'Cards',
   },
-  {
-    path: '/app/charts',
-    icon: 'ChartsIcon',
-    name: 'Charts',
-  },
-  {
-    path: '/app/buttons',
-    icon: 'ButtonsIcon',
-    name: 'Buttons',
-  },
-  {
-    path: '/app/modals',
-    icon: 'ModalsIcon',
-    name: 'Modals',
-  },
-  {
-    path: '/app/tables',
-    icon: 'TablesIcon',
-    name: 'Tables',
-  },
-  {
-    icon: 'PagesIcon',
-    name: 'Pages',
-    routes: [
-      // submenu
-      {
-        path: '/login',
-        name: 'Login',
-      },
-      {
-        path: '/create-account',
-        name: 'Create account',
-      },
-      {
-        path: '/forgot-password',
-        name: 'Forgot password',
-      },
-      {
-        path: '/app/404',
-        name: '404',
-      },
-      {
-        path: '/app/blank',
-        name: 'Blank',
-      },
-    ],
-  },
+  
 ]
 
 export default routes
